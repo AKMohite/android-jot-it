b2aeba41f66ce6fa37419a2da43ea0c6e0b18346
Ashish Mohite <ashishmohite1996@gmail.com>
2021-01-23 20:00:54 +0530

    @Before and @After for testing

--- app/src/androidTest/java/com/ak/jotit/ResourceComparerTest.kt
+++ app/src/androidTest/java/com/ak/jotit/ResourceComparerTest.kt
@@ -3,15 +3,26 @@ package com.ak.jotit
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 class ResourceComparerTest {
 
-    private val resourceComparer = ResourceComparer()
+    private lateinit var resourceComparer: ResourceComparer
+
+    @Before
+    fun setUp() {
+        resourceComparer = ResourceComparer()
+    }
+
+    @After
+    fun tearDown() {
+
+    }
 
     @Test
     fun stringResourcesSameAsGivenString_returnsTrue() {
-
         val context = ApplicationProvider.getApplicationContext<Context>()
         val result = resourceComparer.isEqual(context, R.string.add_task, "Add Task")
 
