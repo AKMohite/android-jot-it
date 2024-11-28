f6a7b61b993e94e9d320b17e8732202cf43db8b1
Ashish Mohite <ashishmohite1996@gmail.com>
2024-11-28 22:19:26 +0530

    Check pure black implementation

--- app/src/main/java/com/ak/jotit/ui/theme/Theme.kt
+++ app/src/main/java/com/ak/jotit/ui/theme/Theme.kt
@@ -1,10 +1,12 @@
 package com.ak.jotit.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 
 private val LightColors = lightColorScheme(
@@ -70,9 +72,13 @@ private val DarkColors = darkColorScheme(
 
 // https://developer.android.com/jetpack/compose/designsystems/material2-material3#m3
 @Composable
-fun JotItTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun JotItTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    pureBlack: Boolean = false, // TODO: Get from preferences
+    content: @Composable() () -> Unit
+) {
     val colors = if (darkTheme) {
-        DarkColors
+        DarkColors.pureBlack(pureBlack)
     } else {
         LightColors
     }
@@ -83,4 +89,15 @@ fun JotItTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable(
         shapes = Shapes,
         content = content
     )
+}
+
+private fun ColorScheme.pureBlack(pureBlack: Boolean): ColorScheme {
+    return if (pureBlack) {
+        this.copy(
+            surface = Color.Black,
+            background = Color.Black
+        )
+    } else {
+        this
+    }
 }
